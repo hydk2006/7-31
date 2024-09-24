1fe79691f316b877ff3d32c82a9c69231ec9eab6
hydk2006 <1759789354@qq.com>
2024-09-24 23:05:28 +0800

    Add files via upload

---
+++ Project4/源.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int score = 0;
+	printf(" 请输入成绩:");
+    scanf("%d", &score);
+	printf("成绩是:%d\n", score);
+	return 0;
+}
---
+++ Project4/源1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+	int age = 10;
+	float goal = 20.0;
+	printf("% d\n", age);
+	_Bool flag1 = true;
+	printf(" %c\n", flag1);
+	printf("%-12.f\n", 123543.1);
+	return 0;
+
+
+}
